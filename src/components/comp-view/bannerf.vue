0e3b4493ed5bb9def05cc0680f80315104b504c4
…zhangyu <2961242927@qq.com>
2020-06-16 20:33:19 +0800

    图表优化

--- src/components/comp-view/bannerf.vue
+++ src/components/comp-view/bannerf.vue
@@ -8,20 +8,20 @@
   </div>
 </template>
 <script>
-import echarts from "echarts"
+import echarts from "echarts";
 export default {
   data() {
-    return {}
+    return {};
   },
   mounted() {
-    this.initialization()
+    this.initialization();
   },
   methods: {
     initialization() {
       // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(document.getElementById("zhu"))
+      var myChart = echarts.init(document.getElementById("zhu"));
       // 绘制图表
-      let dataArr = ["衬衫", "羊毛衫", "雪纺衫", "裤子"]
+      let dataArr = ["衬衫", "羊毛衫", "雪纺衫", "裤子"];
       myChart.setOption({
         // 定义legend图标颜色
         color: ["#f24e06", "#f24e06", "#e2ff1f", "#069dee"],
@@ -31,26 +31,44 @@ export default {
           left: "left",
           top: "middle",
           // legend排列方向
-          orient: "vertical",
+          orient: "vertical"
         },
         grid: {
-          left: "20%",
+          left: "30%"
         },
-        // color:
         title: {
-          text: "ECharts 入门示例",
+          text: "ECharts 入门示例"
         },
         tooltip: {},
         xAxis: {
           data: ["衬衫", "羊毛衫", "雪纺衫", "裤子"],
           type: "category",
-          show: false,
-          axisLine: {
-            show: false,
+          name: "类别",
+          nameLocation: "end",
+          // 坐标轴刻度标签的相关设置
+          axisLabel: {
+            show: false
           },
+          axisLine: {
+            lineStyle: {
+              color: '#718faf'
+            }
+          },
+          axisTick: {
+            show: false
+          }
         },
         yAxis: {
           type: "value",
+          splitLine: {
+            show: false
+          },
+           axisLine: {
+            lineStyle: {
+              color: '#718faf'
+            }
+          },
+          name: "价格"
         },
         series: [
           {
@@ -58,32 +76,39 @@ export default {
             type: "bar",
             data: [5, 20, 36, 10],
             barWidth: "40%",
-            color: ["#f24e06", "#f24e06", "#e2ff1f", "#069dee"],
             // legendHoverLink: true,
+            // itemStyle: {
+            //   barBorderRadius: [10, 10, 0, 0],
+            // },
             itemStyle: {
-              barBorderRadius: [10, 10, 0, 0],
-            },
+              normal: {
+                color: function(params) {
+                  var colorList = ["#f24e06", "#f24e06", "#e2ff1f", "#069dee"];
+                  return colorList[params.dataIndex];
+                }
+              }
+            }
           },
           {
             name: "羊毛衫",
-            type: "bar",
+            type: "bar"
           },
           {
             name: "雪纺衫",
-            type: "bar",
+            type: "bar"
           },
           {
             name: "裤子",
-            type: "bar",
-          },
-        ],
-      })
+            type: "bar"
+          }
+        ]
+      });
       window.addEventListener("resize", function() {
-        myChart.resize()
-      })
-    },
-  },
-}
+        myChart.resize();
+      });
+    }
+  }
+};
 </script>
 <style lang="less" scope="d">
 .bannerf {
