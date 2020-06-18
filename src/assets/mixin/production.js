16ef120b3c3dc8bf96b7feb64f96c82613ca76d2
zhangyu <2961242927@qq.com>
2020-06-18 19:33:21 +0800

    扇形图

--- src/assets/mixin/production.js
+++ src/assets/mixin/production.js
@@ -1,15 +1,18 @@
 export default {
-  data() {
+  data () {
     return {
 
     }
   },
-  mounted() {
+  mounted () {
     this.barWatchInit()
     this.barBot()
+    this.lineInit()
+    this.barBox()
+    this.circularPie()
   },
   methods: {
-    barWatchInit() {
+    barWatchInit () {
       var myEcharts = this.$echarts.init(document.getElementById('barWatch'))
       // 绘制图表
       let dataArr = ["衬衫", "羊毛", "雪纺", "裤子"]
@@ -82,37 +85,37 @@ export default {
           }
         },
         series: [{
-            name: dataArr[0],
-            type: "bar",
-            data: [250, 900, 400, 800, 1000],
-            barWidth: "40%",
-            itemStyle: {
-              normal: {
-                color: function (params) {
-                  return colorArr[params.dataIndex]
-                }
+          name: dataArr[0],
+          type: "bar",
+          data: [250, 900, 400, 800, 1000],
+          barWidth: "40%",
+          itemStyle: {
+            normal: {
+              color: function (params) {
+                return colorArr[params.dataIndex]
               }
             }
-          },
-          {
-            name: "羊毛",
-            type: "bar"
-          },
-          {
-            name: "雪纺",
-            type: "bar"
-          },
-          {
-            name: "裤子",
-            type: "bar"
           }
+        },
+        {
+          name: "羊毛",
+          type: "bar"
+        },
+        {
+          name: "雪纺",
+          type: "bar"
+        },
+        {
+          name: "裤子",
+          type: "bar"
+        }
         ]
       })
       window.addEventListener("resize", function () {
         myEcharts.resize()
       })
     },
-    barBot() {
+    barBot () {
       var myEcharts = this.$echarts.init(document.getElementById('barBot'))
       // 绘制图表
       let dataArr = ["衬衫", "羊毛", "雪纺", "裤子"]
@@ -120,19 +123,23 @@ export default {
         // 定义legend图标颜色
         // legend中的data需要与series中的name一致 才能显示标题
         legend: {
-          show:false
+          show: false
         },
         grid: {
           left: 40,
           right: 40,
           show: false,
           bottom: 10,
-          top: 20,
+          top: 15,
           x: 0,
           y: 0,
           borderWidth: 10
         },
-        tooltip: {},
+        tooltip: {
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
         xAxis: {
           data: ["衬衫", "羊毛衫", "雪纺衫", "裤子"],
           type: "category",
@@ -172,27 +179,250 @@ export default {
           }
         },
         series: [{
-            name: dataArr[0],
-            type: "bar",
-            data: [250, 900, 400, 800, 1000],
-            barWidth: "40%"
+          name: dataArr[0],
+          type: "bar",
+          data: [250, 900, 400, 800, 1000],
+          barWidth: "40%",
+          itemStyle: {//
+            normal: {
+              color: new this.$echarts.graphic.LinearGradient(
+                0, 0, 0, 1,
+                [
+                  { offset: 0, color: '#02bcf8' },
+                  { offset: 1, color: '#00325c ' }
+                ]
+              )
+            },
           },
-          {
-            name: "羊毛",
-            type: "bar"
-          },
-          {
-            name: "雪纺",
-            type: "bar"
-          },
-          {
-            name: "裤子",
-            type: "bar"
-          }
+          // label: {//label要加入normal才可生效,label即为x轴对应Y轴的值
+          //   normal: {
+          //     show: true,
+          //     color: 'red',//设置渐变时候控制不到颜色，只能通过全局textStyle来控制
+          //     position: 'top'
+          //   }
+          // }
+        },
+        {
+          name: "羊毛",
+          type: "bar"
+        },
+        {
+          name: "雪纺",
+          type: "bar"
+        },
+        {
+          name: "裤子",
+          type: "bar"
+        }
         ]
       })
       window.addEventListener("resize", function () {
         myEcharts.resize()
+      })
+    },
+    lineInit () {
+      var echarts = this.$echarts.init(document.getElementById('line'))
+      let legendArr = ['辣椒', '鲜椒']
+      var option = {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#6a7985'
+            }
+          }
+        },
+        legend: {
+          data: legendArr,
+          top: 0,
+          show: true,
+          icon: 'rect',
+          right: 10,
+          textStyle: {
+            color: '#9dacb9'
+          }
+        },
+        // toolbox: {
+        //   feature: {
+        //     saveAsImage: {}
+        //   }
+        // },
+        grid: {
+          left: 10,
+          right: 20,
+          bottom: 5,
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+            axisLabel: {
+              color: '#278d86'
+            },
+
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            max: 500,
+            name: '品种种植面积',
+            nameTextStyle: {
+              color: '#67c3d5',
+              padding: [0, 0, 20, 0]
+            },
+            axisLabel: {
+              color: '#278d86'
+            },
+            axisLine: {
+              show: false,
+              lineStyle: {
+                color: '#248582'
+              }
+            }
+          }
+        ],
+        series: [
+
+          {
+            name: legendArr[0],
+            type: 'line',
+            areaStyle: {},
+            data: [0, 300, 201, 154, 190, 330, 0],
+            smooth: true
+          },
+          {
+            name: legendArr[1],
+            type: 'line',
+            smooth: true,
+            areaStyle: {},
+            data: [0, 200, 301, 400, 300, 330, 0]
+          },
+          {
+            smooth: true,
+            name: '搜索引擎',
+            type: 'line',
+            label: {
+              normal: {
+                show: true,
+                position: 'top'
+              }
+            },
+            areaStyle: {},
+            data: [0, 200, 350, 200, 200, 430, 0]
+          }
+        ]
+      }
+
+      echarts.setOption(option)
+      window.addEventListener("resize", function () {
+        echarts.resize()
+      })
+    },
+    // 鲜椒产品柱状图
+    barBox () {
+      let echarts = this.$echarts.init(document.getElementById('barBox'))
+
+      let option = {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        legend: {
+          data: ['2011年', '2012年'],
+          textStyle: {
+            color: '#b6bfcb'
+          }
+        },
+        grid: {
+          left: '1%',
+          right: '5%',
+          top: 25,
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'value',
+          boundaryGap: [0, 0.01],
+          show: false
+        },
+        yAxis: {
+          type: 'category',
+          data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)'],
+          axisLine: {
+            show: false,
+            lineStyle: {
+              color: '#e7e7eb'
+            }
+          },
+          axisTick: {
+            show: false
+          }
+        },
+        series: [
+          {
+            name: '2011年',
+            type: 'bar',
+            data: [18203, 23489, 29034, 104970, 131744, 630230]
+          },
+          {
+            name: '2012年',
+            type: 'bar',
+            data: [19325, 23438, 31000, 121594, 134141, 681807]
+          }
+        ]
+      }
+
+
+      echarts.setOption(option)
+      window.addEventListener("resize", function () {
+        echarts.resize()
+      })
+    },
+    // 育苗品种
+    circularPie () {
+      let echarts = this.$echarts.init(document.getElementById('circularPie'))
+      let option = {
+        tooltip: {
+          trigger: 'item',
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+        legend: {
+          orient: 'vertical',
+          x: 'right',
+          y: 'bottom',
+          data: ['在线', '离线']
+        },
+        series:
+        {
+
+          name: '访问来源',
+          type: 'pie',
+          radius: ['50%', '70%'],
+          avoidLabelOverlap: false,
+          center: ['40%', '50%'],
+
+          labelLine: {
+            show: true
+          },
+          data: [
+            { value: 335, name: '直接访问' },
+            { value: 310, name: '邮件营销' },
+            { value: 234, name: '联盟广告' },
+            { value: 135, name: '视频广告' },
+            { value: 1548, name: '搜索引擎' }
+          ]
+        }
+
+      }
+      echarts.setOption(option)
+      window.addEventListener("resize", function () {
+        echarts.resize()
       })
     }
   },
--- src/components/comp-view/production.vue
+++ src/components/comp-view/production.vue
@@ -1,49 +1,133 @@
 <template>
   <div class="bannerf">
-    <div class="header"></div>
-    <div class=" flexbox between">
-      <div class="edge-box">
-      <div class="common-sty total-description">
-        <div class="title">
-          <span>总量描述</span>
+    <div class="inner-box">
+      <div class="header">
+        <p>遵义产业大数据分析平台</p>
+        <div class="right-swich">
+          <span>生产</span>
+          <span></span>
         </div>
-        <ul>
-          <li
-            v-for="(item,index) in totalDescription"
-            :key="index"
-            :class="{'active':(index+1)%2===0}"
-          >
-            <span class="icon box1"></span>
-            <span class="icon box2"></span>
-            <span class="content">{{item.title}}</span>
-            <span class="content">{{item.num}}</span>
-          </li>
-        </ul>
       </div>
-      <div class="machining common-sty">
-        <div class="title">
-          <span>加工产品数量</span>
+      <div class="outer-box a-start flexbox between">
+        <div class=" edge-box left">
+          <div class="box-sty total-description">
+            <div class="title">
+              <span>总量描述</span>
+            </div>
+            <ul>
+              <li v-for="(item,index) in totalDescription"
+                  :key="index"
+                  :class="{'active':(index+1)%2===0}">
+                <span class="icon box1"></span>
+                <span class="icon box2"></span>
+                <span class="content">{{item.title}}</span>
+                <span class="content">{{item.num}}</span>
+              </li>
+            </ul>
+          </div>
+          <div class="box-sty machining ">
+            <div class="title">
+              <span>加工产品数量</span>
+            </div>
+            <div class="bar-watch"
+                 id="barWatch"></div>
+          </div>
+          <div class="bot-bar box-sty">
+            <div class="title">
+              <span>农事操作记录</span>
+            </div>
+            <div class="bar-bot"
+                 id="barBot"></div>
+          </div>
         </div>
-        <div class="bar-watch" id="barWatch"></div>
-      </div>
-      <div class="bot-bar common-sty">
-        <div class="title">
-          <span>农事操作记录</span>
+        <div class="map-box">
+          <div class="map"></div>
+          <div class="sub-watch">
+            <div class="circular-pie">
+              <div class="title">
+                <span>育苗品种</span>
+              </div>
+              <div class="pie"
+                   id="circularPie"></div>
+            </div>
+          </div>
         </div>
-        <div class="bar-bot" id="barBot"></div>
+        <div class="edge-box right">
+          <div class="box-sty pie">
+            <div class="title">
+              <span>企业分类</span>
+            </div>
+            <div class="progress flexbox between">
+              <div class="progress-box">
+                <span class="num">130</span>
+                <el-progress type="circle"
+                             color="#ea342e"
+                             :percentage="25"
+                             :width="60"
+                             :show-text="false"></el-progress>
+              </div>
+              <div class="progress-box">
+                <span class="num">130</span>
+                <el-progress type="circle"
+                             :width="60"
+                             color="#eb702f"
+                             :percentage="25"
+                             :show-text="false"></el-progress>
+              </div>
+              <div class="progress-box">
+                <span class="num">130</span>
+                <el-progress type="circle"
+                             color="#f2b348"
+                             :width="60"
+                             :percentage="25"
+                             :show-text="false"></el-progress>
+              </div>
+              <div class="progress-box">
+                <span class="num">130</span>
+                <el-progress type="circle"
+                             color="#00ada2"
+                             :width="60"
+                             :percentage="25"
+                             :show-text="false"></el-progress>
+              </div>
+            </div>
+            <p class="total">企业总数：<span>2013</span></p>
+          </div>
+          <div class="box-sty base">
+            <div class="outer flexbox between">
+              <div class="content-i basef">
+                <p>种植基地</p>
+                <div class="img"></div>
+              </div>
+              <div class="content-i factory">
+                <p>加工厂</p>
+                <div class="img"></div>
+              </div>
+            </div>
+          </div>
+          <div class="box-sty line-outer">
+            <div class="line"
+                 id="line"></div>
+          </div>
+          <div class="box-sty tran-bar">
+            <div class="title">
+              <span>鲜椒产品</span>
+            </div>
+            <div class="bar-box"
+                 id="barBox"></div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="map-box"></div>
     </div>
     <!-- <div class="edge-box"></div> -->
   </div>
 </template>
 <script>
 // import mapMixin from '../../assets/mixin/map'
-import productionEch from "../../assets/mixin/production";
+import productionEch from "../../assets/mixin/production"
 export default {
   mixins: [productionEch],
-  data() {
+  data () {
     return {
       totalDescription: [
         { title: "育苗品种个数", num: 111 },
@@ -57,141 +141,295 @@ export default {
         { title: "加工成品总数量", num: 111 },
         { title: "客商总数量", num: 111 }
       ]
-    };
+    }
   },
-  mounted() {},
+  mounted () { },
   methods: {}
 };
 </script>
 <style lang="less" scoped>
 .bannerf {
   align-items: flex-start;
-  padding: 0 0.125rem 0 0.125rem;
-  box-sizing: border-box;
   height: 100%;
-  background:url('../../assets/img/bgc.jpg') no-repeat center center;
-  .edge-box {
-    width: 25%;
-    height: 100%;
+  background: url('../../assets/img/bgc.jpg') no-repeat center center;
+  background-size: cover;
+  .inner-box {
+    height: 13.5rem;
+    width: 24rem;
     box-sizing: border-box;
-    padding: 0.0375rem 0.125rem;
-    .common-sty {
-      margin: 0 auto;
-      // width: 4rem;
-      // background: rgba(15, 47, 94,.5);
-      margin-bottom: .3625rem;
-
+    padding: 0 0.125rem;
+  }
+  .header {
+    height: 0.8rem;
+    background: url('../../assets/img/header.png') no-repeat center center;
+    background-size: cover;
+    width: 100%;
+    position: relative;
+    .right-swich {
+      position: absolute;
+      right: 0.625rem;
+      top: 50%;
+      color: #adccd7;
+      transform: translateX(-50%);
+      span {
+        display: inline-block;
+        vertical-align: middle;
+        &:nth-child(1) {
+          border: 1px solid #3e537d;
+          border-radius: 0.25rem;
+          width: 1rem;
+          padding: 2px 5px;
+          padding-left: 0.375rem;
+          font-size: 0.225rem;
+          background: url('../../assets/img/icon-productibn.png') no-repeat left
+            center;
+          background-size: 0.225rem 0.225rem;
+          background-position: 0.1rem;
+        }
+        &:nth-child(2) {
+          width: 0.375rem;
+          height: 0.375rem;
+          background: url('../../assets/img/icon-switch.png') no-repeat center
+            center;
+          background-size: cover;
+          margin-left: 0.225rem;
+          cursor: pointer;
+        }
+      }
     }
+    p {
+      text-align: center;
+      color: #e9ecf1;
+      font-size: 0.45rem;
+      padding-top: 0.125rem;
+    }
+  }
+  .outer-box {
+    height: calc(100vh - 1.25rem);
     .title {
       margin-bottom: 10px;
       text-align: right;
+      width: 100%;
       padding-right: 10px;
       span {
         color: #80dbef;
       }
     }
-    .total-description {
-      margin: 0;
-      width:5.4375rem;
-      padding-bottom: .25rem;
-      background: url('../../assets/img/1_03.png') no-repeat center center;
-      background-size:5.275rem 100%;
+    .edge-box {
+      width: 5.4rem;
+      height: 100%;
       box-sizing: border-box;
-      padding-bottom:20px ;
-      ul {
-        width:100% ;
-     li {
-        width: 75%;
-        margin:0 auto ;
-        border-top: 1px solid #0c4a80;
-        &:nth-last-child(1) {
-          border-bottom: 1px solid #0c4a80;
-          &:before {
-            content: "";
-            display: block;
-            width: 2px;
-            height: 2px;
-            border: 2px solid #08669f;
-            background: rgb(15, 47, 94);
-            position: absolute;
-            bottom: -3px;
-            left: -2px;
-          }
-          &:after {
-            content: "";
-            display: block;
-            width: 2px;
-            height: 2px;
-            border: 2px solid #08669f;
-            background: rgb(15, 47, 94);
-            position: absolute;
-            bottom: -3px;
-            right: -2px;
+      padding: 0.0375rem 0.125rem;
+      .box-sty {
+        width: 100%;
+        padding: 0.1875rem;
+        box-sizing: border-box;
+        margin-bottom: 0.1875rem;
+      }
+
+      &.left {
+        .total-description {
+          padding-top: 0.0625rem;
+          padding-bottom: 0.25rem;
+          padding-bottom: 0.25rem;
+          background: url('../../assets/img/1.png') no-repeat center center;
+          background-size: 100% 98%;
+          ul {
+            margin-top: 0.2rem;
+            width: 100%;
+            li {
+              width: 75%;
+              white-space: nowrap;
+              margin: 0 auto;
+              border-top: 1px solid #0c4a80;
+              &:nth-last-child(1) {
+                border-bottom: 1px solid #0c4a80;
+                &:before {
+                  content: '';
+                  display: block;
+                  width: 2px;
+                  height: 2px;
+                  border: 2px solid #08669f;
+                  background: rgb(15, 47, 94);
+                  position: absolute;
+                  bottom: -3px;
+                  left: -2px;
+                }
+                &:after {
+                  content: '';
+                  display: block;
+                  width: 2px;
+                  height: 2px;
+                  border: 2px solid #08669f;
+                  background: rgb(15, 47, 94);
+                  position: absolute;
+                  bottom: -3px;
+                  right: -2px;
+                }
+              }
+              position: relative;
+              background-color: #0f376b;
+              &.active {
+                background-color: rgb(17, 46, 93);
+              }
+              span.icon {
+                display: inline-block;
+                width: 2px;
+                height: 2px;
+                background-color: #0e2d5c;
+                border: 2px solid #08669f;
+                position: absolute;
+                top: -3px;
+                &.box1 {
+                  left: -2px;
+                }
+                &.box2 {
+                  right: -2px;
+                }
+              }
+              .content {
+                display: inline-block;
+                width: 50%;
+                text-align: center;
+                box-sizing: border-box;
+                color: #bcc637;
+                text-decoration: underline;
+
+                &:nth-child(3) {
+                  color: #c4cacb;
+                  padding: 2px 0 2px 10px;
+                  border-right: 1px solid #0c4a80;
+                  text-align: left;
+                }
+              }
+            }
           }
         }
-        position: relative;
-        background-color: #0f376b;
-        &.active {
-          background-color: rgb(17, 46, 93);
-        }
-        span.icon {
-          display: inline-block;
-          width: 2px;
-          height: 2px;
-          background-color: #0e2d5c;
-          border: 2px solid #08669f;
-          position: absolute;
-          top: -3px;
-          &.box1 {
-            left: -2px;
+
+        .machining {
+          background: url('../../assets/img/2.png') no-repeat center center;
+          background-size: cover;
+          .title {
+            padding-top: 0.1625rem;
+            box-sizing: border-box;
           }
-          &.box2 {
-            right: -2px;
+          .bar-watch {
+            width: 4.6625rem;
+            height: 2.25rem;
           }
         }
-        .content {
-          display: inline-block;
-          width: 50%;
-          text-align: center;
-          box-sizing: border-box;
-          color: #bcc637;
-          text-decoration: underline;
-          &:nth-child(3) {
-            color: #c4cacb;
-            padding: 2px 0 2px 10px;
-            border-right: 1px solid #0c4a80;
-            text-align: left;
+        .bot-bar {
+          background: url('../../assets/img/3.png') no-repeat center;
+          background-size: cover;
+          .bar-bot {
+            width: 4.75rem;
+            height: 2.5625rem;
+          }
+        }
+      }
+      &.right {
+        .title {
+          text-align: left;
+          padding-top: 0.125rem;
+        }
+        .box-sty {
+          margin-bottom: 0.1rem;
+        }
+        .pie {
+          background: url('../../assets/img/9.png') no-repeat center center;
+          background-size: 100% 98%;
+          padding-bottom: 0.125rem;
+          padding-top: 0;
+          max-height: 2.1875rem;
+          .progress {
+            padding: 0 5px;
+            overflow: hidden;
+            .progress-box {
+              position: relative;
+              .num {
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                color: #dde4e7;
+                font-size: 20px;
+              }
+              // margin-right: 3px;
+            }
+          }
+          .total {
+            color: #74d1e6;
+            span {
+              color: #f4f4f9;
+            }
+          }
+        }
+        .base {
+          .outer {
+            .content-i {
+              width: 45%;
+              p {
+                color: #85efff;
+                margin: 0.125rem;
+              }
+              .img {
+                width: 2rem;
+                height: 1.5rem;
+                background: url('../../assets/img/no-img.jpg') no-repeat center
+                  center;
+                background-size: 100% 100%;
+              }
+            }
+          }
+        }
+        .line-outer {
+          background: url('../../assets/img/7.png') no-repeat center center;
+          background-size: cover;
+          padding-top: 0.3rem;
+          .line {
+            width: 5.0625rem;
+            height: 2.3rem;
+          }
+        }
+        .tran-bar {
+          background: url('../../assets/img/3.png') no-repeat center center;
+          background-size: 100% 98%;
+          padding-top: 0.125rem;
+          .bar-box {
+            width: 5.0625rem;
+            height: 2.3rem;
           }
         }
       }
     }
-    }
-    .machining {
-      background:url('../../assets/img/2_03.png') no-repeat center center ;
-      background-size: cover;
-      padding:.1875rem ;
-      box-sizing: border-box;
-      .title {
-        padding-top: .1625rem;
-        box-sizing:border-box ;
+    .map-box {
+      width: 12.5rem;
+      .map {
+        height: 7.8125rem;
+        width: 10.3125rem;
       }
-      .bar-watch {
-        width: 5.4375rem;
-        height: 2.5rem;
+      .sub-watch {
+        height: 3.4rem;
+        width: 100%;
+        .circular-pie {
+          width: 5.4375rem;
+          height: 3.275rem;
+          background: url('../../assets/img/4.png') no-repeat center center;
+          background-size: 100% 98%;
+          .title {
+            padding-top: 0.1875rem;
+            padding-right: 0.625rem;
+            box-sizing: border-box;
+          }
+          .pie {
+            width: 4.8375rem;
+            height: 2.5rem;
+          }
+        }
       }
     }
-    .bot-bar {
-      background: url('../../assets/img/3_03.png') no-repeat center;
-      background-size: cover;
-      .bar-bot {
-        width: 5.4375rem;
-        height: 3.25rem;
-      }
-    }
-  }
-  .map-box {
-    flex: 1;
-    height: 100%;
   }
 }
+</style>
+<style lang="less">
 </style>
